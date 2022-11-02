6f6ea67073286087edd75b822babfa13b03ba01d
Daniel Oh <ohdoyoel@gmail.com>
2022-11-02 15:40:07 +0900

    [add] inputField ui & function complete

---
+++ FlirtingChatBotProject/Assets/2. Scripts/Chat/DebugChat.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(VisualChatManager))]
+public class DebugChat : Editor
+{
+    VisualChatManager visualChatManager;
+    string text;
+
+    void OnEnable()
+    {
+        visualChatManager = target as VisualChatManager;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        EditorGUILayout.BeginHorizontal();
+        text = EditorGUILayout.TextArea(text);
+
+        if (GUILayout.Button("보내기", GUILayout.Width(60)) && text.Trim() != "")
+        {
+            visualChatManager.VisualizeChat(true, text, "유붕이", null);
+            text = "";
+            GUI.FocusControl(null);
+        }
+
+        if (GUILayout.Button("받기", GUILayout.Width(60)) && text.Trim() != "")
+        {
+            visualChatManager.VisualizeChat(false, text, "유순이", Resources.Load<Texture2D>("Assets/1. Resources/Images/profile_img.png"));
+            text = "";
+            GUI.FocusControl(null);
+        }
+
+        EditorGUILayout.EndHorizontal();
+    }
+}
---
+++ FlirtingChatBotProject/Assets/2. Scripts/Chat/InputFieldChat.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputFieldChat : MonoBehaviour
+{
+    public InputField inputField;
+    public VisualChatManager visualChatManager;
+    public Button inputButton;
+    private bool inputAble = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inputAble)
+        {
+            inputField.ActivateInputField();
+            if (inputField.text != "")
+            {
+                inputButton.GetComponent<Image>().color = new Color(1.0f, 0.89f, 0.0f, 1.0f);
+            }
+        }
+        else
+        {
+            inputButton.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+        }
+
+        // 2. if text arrived, then visualize the text
+    }
+
+    public void InputDone()
+    {
+        // 1. call chatbot api bot text from given inputField.text
+
+        visualChatManager.VisualizeChat(true, inputField.text, "유붕이", null); 
+        inputField.text = "";
+
+        inputField.DeactivateInputField();
+        inputAble = false;
+        StartCoroutine(WaitForIt()); // input 가능 여부 flag 처리
+    }
+
+    IEnumerator WaitForIt()
+    {
+        yield return new WaitForSeconds(0.5f);
+        inputAble = true;
+    }
+}
